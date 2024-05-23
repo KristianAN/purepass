606b091ea1cd5ce96f1b9c1c0d4debb54b5e0f44
Kristian Nedrevold-Hansen <kristian@krined.no>
2024-05-23 22:17:11 +0200

    Added word file resource and read it to pw generator

--- src/PasswordGenerator.hs
+++ src/PasswordGenerator.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module PasswordGenerator (generateForRandom, PwFlags (..), RandomPw (..)) where
 
 import Control.Monad (replicateM)
+import Data.Aeson
+import Data.ByteString.Lazy qualified as BL
 import Data.Functor
+import Data.Text
+import GHC.Generics
+import Paths_purepass (getDataFileName)
 import System.Random
 
 data WordPw = WordPw
@@ -13,6 +21,16 @@ data RandomPw = RandomPw
   { rLen :: Int,
     special :: Bool
   }
+
+newtype WordList = WordList {words :: [String]} deriving (Generic, Show)
+
+instance FromJSON WordList
+
+getWordFile :: IO (Maybe WordList)
+getWordFile = do
+  path <- getDataFileName "resources/words.json"
+  content <- BL.readFile path
+  pure (decode content :: Maybe WordList)
 
 data PwFlags
   = WordPwFlag WordPw
@@ -27,8 +45,6 @@ generateForRandom _ = undefined
 randomChar :: String -> IO Char
 randomChar input = do
   r <-
-    let len = length input - 1
+    let len = Prelude.length input - 1
      in randomRIO (0, len)
   pure $ input !! r
-
-
